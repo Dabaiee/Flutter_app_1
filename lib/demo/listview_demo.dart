dd189ec6383342ecf43fef40a302e0777e3da85b
dabaie <tongzhou1212@hotmail.com>
2019-07-09 20:31:46 +0800

    Jul9 drawer

---
+++ lib/demo/listview_demo.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class ListViewDemo extends StatelessWidget{
+  @override
+
+  Widget _listItemBuilder(BuildContext context, int index){
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0,),
+          Text(
+              posts[index].title,
+              style: Theme.of(context).textTheme.title
+          ),
+          Text(
+              posts[index].author,
+              style: Theme.of(context).textTheme.subhead
+          ),
+          SizedBox(height: 16.0,),
+        ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ListView.builder(
+      itemCount: posts.length,
+      itemBuilder: _listItemBuilder,
+    );
+  }
+}
